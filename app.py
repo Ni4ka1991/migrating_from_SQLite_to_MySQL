50655f1a4c5e554091c2253694f9ec08e6c38541
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-03-17 15:45:08 +0200

    some changes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import sqlite3
+import mysql.connector
+
+
 #from config import *
 
 base = sqlite3.connect( "data/dnsmgr.db" )
@@ -18,3 +21,21 @@ cursor = base.cursor()
 
 request = cursor.execute( "SELECT * FROM record" ).fetchall()
 print( request )
+
+
+mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Rekt,zrf_2020",
+        database="new.bd"
+        )
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute( sql, val )
+
+mydb.commit()
+
+print( mycursor.rowcount, "record inserted." )
