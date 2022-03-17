49080f6e9696ced2cf6a9fa98fcc27dc4a488fb6
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-17 13:11:06 +0200

    Add files

--- app.py
+++ app.py
@@ -2,12 +2,18 @@
 
 import sqlite3
 
-base = sqlite3.connect( "new.db" )
+base = sqlite3.connect( "data/new.db" )
 cursor = base.cursor()
+
 base.execute( "CREATE TABLE IF NOT EXISTS {} (login, password)".format( "data" ))
 base.commit()
 
 cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "antonii2014", "qwerty1234"))
 base.commit()
+
 cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "pawlik1981", "qwerty4321"))
 base.commit()
+
+
+request = cursor.execute( "SELECT * FROM data" ).fetchall()
+print( request )
