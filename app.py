ee71864446a51ef87152b853175e08254381e96b
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-17 14:05:29 +0200

    Print data from table result

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 import sqlite3
+#from config import *
 
-base = sqlite3.connect( "data/new.db" )
+base = sqlite3.connect( "data/dnsmgr.db" )
 cursor = base.cursor()
 
-base.execute( "CREATE TABLE IF NOT EXISTS {} (login, password)".format( "data" ))
-base.commit()
+#base.execute( "CREATE TABLE IF NOT EXISTS {} (login, password)".format( "data" ))
+#base.commit()
 
-cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "antonii2014", "qwerty1234"))
-base.commit()
+#cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "antonii2014", "qwerty1234"))
+#base.commit()
 
-cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "pawlik1981", "qwerty4321"))
-base.commit()
+#cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "pawlik1981", "qwerty4321"))
+#base.commit()
 
 
-request = cursor.execute( "SELECT * FROM data" ).fetchall()
+request = cursor.execute( "SELECT * FROM record" ).fetchall()
 print( request )
