5d35dbc94eda1052dc1b8087d83e2d47c81337c4
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-20 22:04:13 +0200

    View all data

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python3
 
 import sqlite3
-import mysql.connector
+import mysql.connector as mqlc
+from config import *
 
 
-#from config import *
-
-base = sqlite3.connect( "data/dnsmgr.db" )
+base = sqlite3.connect( f"data/{db_from_name}" )
 cursor = base.cursor()
-
-#base.execute( "CREATE TABLE IF NOT EXISTS {} (login, password)".format( "data" ))
-#base.commit()
-
-#cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "antonii2014", "qwerty1234"))
-#base.commit()
-
-#cursor.execute( "INSERT INTO data VALUES( ?, ? )", ( "pawlik1981", "qwerty4321"))
-#base.commit()
-
-request = cursor.execute( "SELECT * FROM record" ).fetchall()
-print( request )
+cursor.execute( "SELECT * FROM domain" )
+request = cursor.fetchall()
 
 
-mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Rekt,zrf_2020",
-        database="new.bd"
-        )
+k = 1
+for row in request:
+    print( f"id = {row[0]}; name = {row[1]}; dtype = {row[5]}" )
+    k += 1
 
-mycursor = mydb.cursor()
 
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("John", "Highway 21")
-mycursor.execute( sql, val )
+mydb = mqlc.connect( host="localhost", user="root", password="qwerty258", database="information_schema" )
+cursor = mydb.cursor()
+cursor.execute( "SELECT * FROM ALL_PLUGINS" )
+request_mysql = cursor.fetchall()
 
-mydb.commit()
-
-print( mycursor.rowcount, "record inserted." )
+'''
+k = 1
+for row in request_mysql:
+    print( f"{k} >>> {row}" )
+    k += 1
+'''
