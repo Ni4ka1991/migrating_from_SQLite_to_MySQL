ba5e803d0cdac0137f423d2e44139f59a1443d37
Ni4ka1991 <astahova_nica@mail.ru>
2022-03-21 12:21:58 +0200

    Add connection_error function

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
 
 import sqlite3
+from sqlite3 import Error
 import mysql.connector as mqlc
 from config import *
 
+path = f"data/{db_from_name}"
 
-base = sqlite3.connect( f"data/{db_from_name}" )
+def create_connection( path ):
+    connection = None
+    try:
+        connection = sqlite3.connect( path )
+        print("Connection to SQLite DB successful")
+    except Error as err:
+        print(f"The error '{err}' occurred")
+
+    return connection
+
+base = create_connection( path )
 cursor = base.cursor()
-cursor.execute( "SELECT * FROM domain" )
-request = cursor.fetchall()
+a = cursor.execute( "SELECT name, dtype FROM domain" )
+#cursor.execute( "SELECT * FROM record" )
+print( a.fetchall() )
 
 
-k = 1
-for row in request:
-    print( f"id = {row[0]}; name = {row[1]}; dtype = {row[5]}" )
-    k += 1
 
 
-mydb = mqlc.connect( host="localhost", user="root", password="qwerty258", database="information_schema" )
-cursor = mydb.cursor()
-cursor.execute( "SELECT * FROM ALL_PLUGINS" )
-request_mysql = cursor.fetchall()
+###### mysql ###
+try:
+    mydb = mqlc.connect( host="localhost", user="root", password="qwerty258", database="information_schema" )
+    print("Connection to Mariadb DB successful")
+    cursor = mydb.cursor()
+    cursor.execute( "SELECT * FROM ALL_PLUGINS" )
+except mqlc.Error as err:
+    print( f"Are you forget to srart mariadb server?: {err}" )
 
-'''
-k = 1
-for row in request_mysql:
-    print( f"{k} >>> {row}" )
-    k += 1
-'''
+
