1168284550a0a5fa937384e015234e14c8fc00f7
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-03-24 15:37:40 +0200

    some changes

--- app.py
+++ app.py
@@ -11,25 +11,25 @@ def create_connection_SQLite( path ):
     connection = None
     try:
         connection = sqlite3.connect( path )
-        print("Connection to SQLite DB successful")
+        print("Hi, connection to SQLite DB successful")
     except Error as err:
         print(f"The error '{err}' occurred")
 
     return connection
 
-'''
+
 def create_connection_MySQL( host_name, user_name, user_password, database ):
     connection = None
     try:
         mydb = mqlc.connect( host = host_name, user = user_name, password = user_password, database = database_name )
-        print("Connection to Mariadb DB successful")
+        print("Hi, connection to Mariadb DB successful )))")
         cursor = mydb.cursor()
         cursor.execute( "SELECT * FROM ALL_PLUGINS" )
     except mqlc.Error as err:
         print( f"Are you forget to srart mariadb server?: {err}" )
     
     return connection
-'''
+
 
 def create_execute( connection, query ):
         cursor = connection.cursor()
@@ -45,15 +45,15 @@ def create_execute( connection, query ):
 base_SQLite = create_connection_SQLite( path )
 a = create_execute( base_SQLite, "SELECT name, dtype FROM domain" )
 print( a )
-
-base_MySQL = create_connection_MySQL( host_name, user_name, user_password, "information_schema" )
 '''
+base_MySQL = create_connection_MySQL( host_name, user_name, user_password, "information_schema" )
+b = create_execute( base_MySQL, "SELECT * ALL_PLUGINS" )
 
 
-#mydb = mqlc.connect( host = host_name, user = user_name, password = user_password, database = database_name )
-mydb = mqlc.connect( host = "localhost", user = "root", password = "Rekt,zrf_2020", database = "information_schema" )
+'''
+#mydb = mqlc.connect( host = "localhost", user = "root", password = "qwerty258", database = "information_schema" )
 #cursor = mydb.cursor()
 #cursor.execute( "SELECT * FROM ALL_PLUGINS" )
 #request_mysql = cursor.fetchall()
 #print( request_mysql )
-
+'''
